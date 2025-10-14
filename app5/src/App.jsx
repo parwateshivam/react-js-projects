b5c8cf3981e03fb488057207b25be7b0cf895b14
shivamparwate <shivamparvate61@gmail.com>
2025-10-14 21:57:06 +0530

    useEffect hook

--- app5/src/App.jsx
+++ app5/src/App.jsx
@@ -1,32 +1,48 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AddLanguage from "./components/AddLanguage"
 import ShowLanguages from "./components/ShowLanguages"
 import { languages } from "./data/languages.js"
-// import axios from "axios"
+import { fetchData } from "../api/test.js"
 
 function App() {
 
-  // async function fetchData() {
-  //   try {
-  //     let result = await axios({
-  //       method : "GET",
-  //       url : "http://localhost:7001/test/hello"
-  //     })
-  //     console.log(result.data)
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
+  // let [languageData, setLanguageData] = useState(languages)
 
-  let [languageData, setLanguageData] = useState(languages)
+  let [responseData, setResponseData] = useState(null)
+
+  useEffect(() => {
+    async function getTestData() {
+      try {
+        let response = await fetchData()
+        setResponseData(response.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getTestData()
+  }, [])
 
   return (
     <>
+      <div className='flex justify-center items-center gap-20'>
+        {responseData ? <div>
+          <h1>
+            message: {responseData.message}
+          </h1>
+          <div>
+            <ul>
+              <li>name : {responseData.userData.name}</li>
+              <li>email : {responseData.userData.email}</li>
+            </ul>
+          </div>
+        </div> : null
+        }
+      </div>
       {/* <div className="flex justify-center my-10">
         <button onClick={fetchData} className="bg-green-500 p-2 rounded-md text-center">Get Data</button>
       </div> */}
-      <AddLanguage languageData={languageData} setLanguageData={setLanguageData} />
-      <ShowLanguages languageData={languageData} setLanguageData={setLanguageData} />
+      {/* <AddLanguage languageData={languageData} setLanguageData={setLanguageData} />
+      <ShowLanguages languageData={languageData} setLanguageData={setLanguageData} /> */}
     </>
   )
 }
